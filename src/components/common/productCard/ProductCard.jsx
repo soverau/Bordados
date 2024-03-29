c0af7927c46fdb6e072fa8a51d4fe14db6a846df
Vicky Oehm <vickyoehm@yahoo.com.ar>
2024-03-28 22:44:02 -0300

    Destructured props in ProductCard

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -1,11 +1,12 @@
 export const ProductCard = (props) => {
+  const { image, titulo, descripcion } = props;
   return (
     <div className="col gx-5 gy-5">
       <div className="card">
-        <img src={props.image} className="card-img-top" alt="..." />
+        <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{props.titulo}</h5>
-          <p className="card-text">{props.descripcion} description</p>
+          <h5 className="card-title">{titulo}</h5>
+          <p className="card-text">{descripcion} description</p>
           <a href="#" className="btn btn-primary">
             Comprar
           </a>
