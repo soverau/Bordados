96cfcc4d075f7bf53a2ce2edbde327b44548b7dc
Vicky Oehm <vickyoehm@yahoo.com.ar>
2024-04-24 01:24:45 -0300

    Removed Comments

--- src/components/pages/itemListContainer/ItemListContainer.jsx
+++ src/components/pages/itemListContainer/ItemListContainer.jsx
@@ -1,9 +1,7 @@
-// import { ProductCard } from "../../common/productCard/ProductCard";
-// import Product1 from "/src/assets/images/DMC_CATEGORIES_16x9_Ratio_DESKTOP-MOBILE_720px_GOS_KN-1.jpg";
 import { useEffect, useState } from "react";
 import { products } from "../../productsMock.js";
 import ItemList from "./ItemList.jsx";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
@@ -38,45 +36,5 @@ const ItemListContainer = () => {
   console.log(error);
   return <ItemList items={items} error={error} />;
 };
-
-// const ItemListContainer = ({ greeting }) => {
-//   return (
-//     <div className="container">
-//       <h1 className="p-5">{greeting}</h1>
-//       <div className="container">
-//         <div className="row">
-//           <ProductCard
-//             image={Product1}
-//             titulo={"Kits"}
-//             descripcion={
-//               "Some quick example text to build on the card title and make up the bulk of the card content."
-//             }
-//           />
-//           <ProductCard
-//             image={Product1}
-//             titulo={"Patrone"}
-//             descripcion={
-//               "Some quick example text to build on the card title and make up the bulk of the card content."
-//             }
-//           />
-//           <ProductCard
-//             image={Product1}
-//             titulo={"Hilos"}
-//             descripcion={
-//               "Some quick example text to build on the card title and make up the bulk of the card content."
-//             }
-//           />
-//           <ProductCard
-//             image={Product1}
-//             titulo={"Accesorio"}
-//             descripcion={
-//               "Some quick example text to build on the card title and make up the bulk of the card content."
-//             }
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default ItemListContainer;
